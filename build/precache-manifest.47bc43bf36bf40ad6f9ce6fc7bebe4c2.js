2c79ee85133c2461241de226f82b156bad70b3ab
Joseaph Mankin <JoseaphMankin@gmail.com>
2018-12-20 21:22:01 -0600

    Yarn Build Experiment

---
+++ build/precache-manifest.47bc43bf36bf40ad6f9ce6fc7bebe4c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5157f51a45acf482dc1604c7ee7961db",
+    "url": "/static/media/CastlevaniaBig.5157f51a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c9524c01c25494be0b42",
+    "url": "/static/js/main.c9524c01.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c9524c01c25494be0b42",
+    "url": "/static/css/main.4b066610.chunk.css"
+  },
+  {
+    "revision": "d62300a8ef4ec31415e564a6de2690da",
+    "url": "/index.html"
+  }
+];
